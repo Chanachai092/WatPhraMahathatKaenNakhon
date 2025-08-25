dfc992f8b034096ac88a545a4b595df04efca104
Connie1125 <botkung0925432494@gmail.com>
2025-08-25 11:57:24 +0700

    18

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import WatInfo from '../components/WatInfo.vue'
 import WatHistory from '../components/WatHistory.vue'
 import TouristInfo from '../components/TouristInfo.vue'
-import PanoramaViewer from '../components/PanoramaViewer.vue' // 👈 ต้อง import
+import PanoramaViewer from '../components/PanoramaViewer.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/info', component: WatInfo },
   { path: '/history', component: WatHistory },
   { path: '/tourist', component: TouristInfo },
-  { path: '/panorama', component: PanoramaViewer }, // ✅ เพิ่ม path นี้
+  { path: '/panorama', component: PanoramaViewer },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
